8b87f66cbb1918e0bcc555b4d511b99fad9d479d
larimar1212 <larimarfondeur@gmail.com>
2022-07-19 12:00:41 -0400

    solution

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+const employee = {
+ /*[key]*/ name: 'Lari',
+/* .value*/ streetAddress: '56 Main St'
+}
+
+
+// returning a new {} of a new employee non des
+function updateEmployeeWithKeyAndValue(employee, key, value) { // three parametets
+    const newEmployee = {...employee}; // spread to add and return new
+    newEmployee[key] = value
+    return newEmployee
+
+}
+
+// return mutated object passed in
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value 
+    return employee
+}
+
+// delete and not mutate original
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee;
+
+}
+
+console.log(destructivelyDeleteFromEmployeeByKey)
